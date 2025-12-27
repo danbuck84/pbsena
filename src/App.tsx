6c9168ecdc3ffa097bbbb7c9a3e504e50bd423c5
Daniel Buck <danbuck@Daniels-MacBook-Air.local>
2025-12-27 11:41:54 -0300

    fix: app router and vite base config

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Login } from './pages/Auth/Login';
 
+import { Home } from './pages/Home';
+
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/login" element={<Login />} />
+                <Route path="/dashboard" element={<Home />} />
                 {/* Default route redirects to Login for now */}
+                <Route path="/" element={<Navigate to="/login" replace />} />
                 <Route path="*" element={<Navigate to="/login" replace />} />
             </Routes>
         </BrowserRouter>
